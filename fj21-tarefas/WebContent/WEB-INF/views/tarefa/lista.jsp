faea8a2f3d2915a3668ff640fbad82b544364a63
Fernando <fernando.de.souza@gmail.com>
2016-08-24 22:05:19 -0300

    Exemplo spring caelum

---
+++ fj21-tarefas/WebContent/WEB-INF/views/tarefa/lista.jsp
@@ -0,0 +1,72 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Lista de Tarefas</title>
+	<script type="text/javascript" src="resources/js/jquery.js"></script>
+	<script type="text/javascript">
+		function finalizarAgora(id) {
+			$.post("finalizaTarefa",{'id' : id} ,function(resposta){
+				$("#tr_tarefa_"+id).html(resposta);
+			})
+		}
+		function removeTarefa(id) {
+			$.post("removeTarefa",{'id' : id} ,function(){
+				$("#tr_tarefa_"+id).hide();
+			})
+		}
+	</script>
+</head>
+<body>
+	<a href="adicionaTarefa">Criar nova tarefa</a>
+	
+	<br /><br />
+	
+	<table border="1">
+		<tr>
+			<td>Id</td>
+			<td>Descrição</td>
+			<td>Finalização</td>
+			<td>Data de Finalização</td>
+			<td>Remover</td>
+			<td>Alterar</td>
+		</tr>
+		
+		<c:forEach items="${tarefas}" var="tarefa">
+			<tr id="tr_tarefa_${tarefa.id}">
+				<td>
+					${tarefa.id}
+				</td>
+				<td>
+					${tarefa.descricao}
+				</td>
+				<c:if test="${tarefa.finalizado eq false}">
+					<td id="tarefa_${tarefa.id}">
+						<a href="#" onclick="finalizarAgora(${tarefa.id})">
+							Finalizar Agora!
+						</a>
+					</td>
+				</c:if>
+				<c:if test="${tarefa.finalizado eq true}">
+					<td>
+						Finalizado
+					</td>
+				</c:if>
+				<td>
+					<fmt:formatDate value="${tarefa.dataFinalizacao.time}" pattern="dd/MM/yyyy"/>
+				</td>
+				<td>
+					<a href="#" onclick="removeTarefa(${tarefa.id})">Remover</a>
+				</td>
+				<td>
+					<a href="mostraTarefa?id=${tarefa.id}">Alterar</a>
+				</td>
+		</c:forEach>
+	</table>
+</body>
+</html>
---
+++ fj21-tarefas/src/br/com/caelum/tarefas/controller/TarefasController.java
@@ -0,0 +1,76 @@
+package br.com.caelum.tarefas.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.caelum.tarefas.dao.JdbcTarefaDao;
+import br.com.caelum.tarefas.modelo.Tarefa;
+
+@Controller
+public class TarefasController {
+	
+	@RequestMapping("novaTarefa")
+	public String form(){
+		return "tarefa/formulario";
+	}
+	
+	@RequestMapping("adicionaTarefa")
+	public String adiciona(@Valid Tarefa tarefa, BindingResult result){
+		
+		if(result.hasFieldErrors()){
+			return "tarefa/formulario";
+		}
+		
+		JdbcTarefaDao dao = new JdbcTarefaDao();
+		dao.adiciona(tarefa);
+		//return "tarefa/adicionada";
+		return "redirect:listaTarefas";
+	}
+	
+	@RequestMapping("listaTarefas")
+	public ModelAndView lista(){
+		JdbcTarefaDao dao = new JdbcTarefaDao();
+		ModelAndView mav = new ModelAndView("tarefa/lista");
+		mav.addObject("tarefas", dao.lista());
+		//model.addAttribute("tarefas", dao.lista());
+		//return "tarefa/lista";
+		return mav;
+	}
+	
+	@ResponseBody
+	@RequestMapping("removeTarefa")
+	public void remove(Tarefa tarefa){
+		JdbcTarefaDao dao = new JdbcTarefaDao();
+		dao.remove(tarefa);
+		System.out.println("Tarefa removida -> " + tarefa.getId());
+	}
+	
+	@RequestMapping("mostraTarefa")
+	public String mostra(Long id, Model model){
+		JdbcTarefaDao dao = new JdbcTarefaDao();
+		model.addAttribute("tarefa", dao.buscaPorId(id));
+		return "tarefa/mostra";
+	}
+	
+	@RequestMapping("alteraTarefa")
+	public String altera(Tarefa tarefa){
+		JdbcTarefaDao dao = new JdbcTarefaDao();
+		dao.altera(tarefa);
+		return "redirect:listaTarefas";
+	}
+
+	@RequestMapping("finalizaTarefa")
+	public String finaliza(Long id, Model model){
+		JdbcTarefaDao dao = new  JdbcTarefaDao();
+		dao.finaliza(id);
+		model.addAttribute("tarefa",dao.buscaPorId(id));
+		System.out.println("Tarefa finalizada -> " + id);
+		return "tarefa/finalizada";
+	}
+}
